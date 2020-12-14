9f006ea0b3fb016293919a98bfc634ffd596ac3e
wicver <wicusverhoef@gmail.com>
2020-12-14 01:06:00 +0000

    Cypher Transaction Blocks work 003

--- serpentmonkee/CypherQueue.py
+++ serpentmonkee/CypherQueue.py
@@ -17,22 +17,60 @@ class CypherQueue:
 
 
 class CypherQueues:
-    def __init__(self, redisClient, cQueues):
+    def __init__(self, redisClient, cQueues, workingQ, completedQ):
         self.cQueues = cQueues
+        self.workingQ = workingQ
+        self.completedQ = completedQ
         self.redisClient = redisClient
         self.cQNames = []
         for q in self.cQueues:
             self.cQNames.append(q.queueName)
 
+    def getQLens(self):
+        lenString = ""
+        for q in self.cQNames:
+            l = self.redisClient.llen(q)
+            lenString += f'Q={q} len={l}, \n'
+
+        lenString += f'Q=Working len={self.redisClient.llen(self.workingQ.queueName)}, \n'
+        lenString += f'Q=Completed len={self.redisClient.llen(self.completedQ.queueName)}, \n'
+        print(lenString)
+
     def pushCypherQueryToQueue(self, ctb, queueName):
         serial_ = json.dumps(ctb.instanceToSerial(), cls=um.RoundTripEncoder)
         self.redisClient.rpush(queueName, serial_)
+
+    def pushCtbToWorkingQ(self, ctbSerial, isLeft=False):
+        if isLeft:
+            self.redisClient.lpush(self.workingQ.queueName, ctbSerial)
+        else:
+            self.redisClient.rpush(self.workingQ.queueName, ctbSerial)
+
+    def removeCtbFromWorkingQ(self, ctbSerial):
+        removed = self.redisClient.lrem(self.workingQ.queueName, 0, ctbSerial)
+        return removed
+
+    def popCtbSerialFromWorkingQ(self):
+        """
+        Fetches (LPOP) the next ctb from the queues
+        """
+        popped = self.redisClient.blpop(
+            self.workingQ.queueName, 0.01)
+        # popped = self.redisClient.lpop(self.queueName)
+
+        if not popped:
+            print("WorkingQ IS EMPTY_________________________________________")
+        else:
+            dataFromRedis = json.loads(popped[1], cls=um.RoundTripDecoder)
+
+            #print(f"Data read from WORKING Q:{dataFromRedis}")
+            return dataFromRedis
+        return None
 
     def pushCtbToWaitingQ(self, ctBlock):
         """
         Pushes (RPUSH) the given ctb back to one of the queues
         """
-        ctBlock.numRetries += 1
         ctBlock.lastUpdatedAt = datetime.now(timezone.utc)
         if len(self.cQueues) == 3:
             if ctBlock.priority == 'H':
@@ -48,3 +86,10 @@ class CypherQueues:
                 self.pushCypherQueryToQueue(ctBlock, self.cQueues[1].queueName)
         elif len(self.cQueues) >= 1:
             self.pushCypherQueryToQueue(ctBlock, self.cQueues[0].queueName)
+
+    def pushCtbToCompletedQ(self, ctBlock):
+        """
+        Pushes (RPUSH) the given ctb back to one of the queues
+        """
+        ctBlock.lastUpdatedAt = datetime.now(timezone.utc)
+        self.pushCypherQueryToQueue(ctBlock, self.completedQ.queueName)
--- serpentmonkee/CypherTransactionBlock.py
+++ serpentmonkee/CypherTransactionBlock.py
@@ -8,7 +8,7 @@ import random
 import uuid
 import copy
 from serpentmonkee import UtilsMonkee as um
-from neo4j.exceptions import CypherSyntaxError, ServiceUnavailable
+from neo4j.exceptions import CypherSyntaxError, ServiceUnavailable, ClientError
 
 # --------------------------------------------------------------------
 
@@ -26,6 +26,9 @@ class CypherTransactionBlock:
         self.statements = statements
         self.transactionUid = transactionUid
         self.origin = origin
+        self.runTime = None
+        self.status = None
+        self.errors = None
 
         self.setJson()
 
@@ -37,14 +40,20 @@ class CypherTransactionBlock:
             "lastUpdatedAt": self.lastUpdatedAt,
             "uid": self.transactionUid,
             "origin": self.origin,
-            "statements": self.statements
+            "statements": self.statements,
+            "runTime": self.runTime,
+            "status": self.status,
+            "errors": self.errors
         }
 
     def instanceToSerial(self):
         return json.dumps(self.json, cls=um.RoundTripEncoder)
 
     def makeFromSerial(self, serial):
-        dict_ = json.loads(serial, cls=um.RoundTripDecoder)
+        if not isinstance(serial, dict):
+            dict_ = json.loads(serial, cls=um.RoundTripDecoder)
+        else:
+            dict_ = serial
         self.priority = um.getval(dict_, "priority")
         self.numRetries = um.getval(dict_, "numRetries")
         self.createdAt = um.getval(dict_, "createdAt")
@@ -52,6 +61,10 @@ class CypherTransactionBlock:
         self.transactionUid = um.getval(dict_, "uid")
         self.origin = um.getval(dict_, "origin")
         self.statements = um.getval(dict_, "statements")
+        self.runTime = um.getval(dict_, "runTime")
+        self.status = um.getval(dict_, "status")
+        self.errors = um.getval(dict_, "errors")
+        self.setJson()
 
 
 class CypherTransactionBlockWorker:
@@ -65,43 +78,111 @@ class CypherTransactionBlockWorker:
         Fetches (LPOP) the next ctb from the queues
         """
         popped = self.cypherQueues.redisClient.blpop(
-            self.cypherQueues.cQNames, 1)
+            self.cypherQueues.cQNames, 0.01)
         # popped = self.redisClient.lpop(self.queueName)
 
         if not popped:
             print("QUEUES ARE EMPTY_________________________________________")
+        else:
+            dataFromRedis = json.loads(popped[1], cls=um.RoundTripDecoder)
 
-        dataFromRedis = json.loads(popped[1], cls=um.RoundTripDecoder)
-        print(f"Data read from Q:{dataFromRedis}")
-        ctb = CypherTransactionBlock(None, None, None, None)
-        ctb.makeFromSerial(dataFromRedis)
-        return ctb
+            # print(f"Data read from Q:{dataFromRedis}")
+            ctb = CypherTransactionBlock(None, None, None, None)
+            ctb.makeFromSerial(dataFromRedis)
+            if ctb.statements:
+                ctb.lastUpdatedAt = datetime.now(timezone.utc)
+                ctb.setJson()
+                self.pushBlockToWorkingQueue(ctb.instanceToSerial())
+            return ctb
+
+    def pushBlockToWorkingQueue(self, ctbSerial, isLeft=False):
+        self.cypherQueues.pushCtbToWorkingQ(ctbSerial, isLeft)
+
+    def removeBlockFromWorkingQueue(self, ctbSerial):
+        rem = self.cypherQueues.removeCtbFromWorkingQ(ctbSerial)
+        print(f' {rem} instances removed from workingQ')
+
+    def popBlockFromWorkingQueue(self):
+        ser = self.cypherQueues.popCtbSerialFromWorkingQ()
+        if ser:
+            ctb = CypherTransactionBlock(None, None, None, None)
+            ctb.makeFromSerial(ser)
+            return ctb
+        return None
+
+    def lookForExpiredWorkingBlocks(self, expiryInSeconds=90):
+        wb = self.popBlockFromWorkingQueue()
+        if wb:
+
+            if wb.statements:
+
+                timeDiff = um.dateDiff('s', wb.lastUpdatedAt,
+                                       datetime.now())
+                print(f' Age of workingQ item={timeDiff}')
+                if timeDiff >= expiryInSeconds:
+                    # self.removeBlockFromWorkingQueue(wb.instanceToSerial())
+                    wb.lastUpdatedAt = datetime.now()
+                    wb.numRetries += 1
+                    self.cypherQueues.pushCtbToWaitingQ(wb)
+                else:
+                    wb.setJson()
+                    wb_serial = wb.instanceToSerial()
+                    self.pushBlockToWorkingQueue(wb_serial, isLeft=True)
 
     def executeBlock(self, ctBlock: CypherTransactionBlock):
         """
         Executes all statments in the ctb as one transaction.
         Returns success boolean
         """
-        try:
-            with self.neoMonkee.neoDriver.session() as session:
-                results = session.write_transaction(
-                    self._statementsAsTransaction, ctBlock.statements)
-                print(results)
-                return True
-        except CypherSyntaxError as e:
-            print('CypherSyntaxError')
-            print(repr(e))
-            self.cypherQueues.pushCtbToWaitingQ(
-                ctBlock)  # TEMP! just for testing
-            return False
-        except ServiceUnavailable as e:
-            print('ServiceUnavailable')
-            print(repr(e))
-            self.cypherQueues.pushCtbToWaitingQ(ctBlock)
-            return False
-        except Exception as e:
-            print(repr(e))
-            return False
+        if ctBlock.statements:
+            try:
+                with self.neoMonkee.neoDriver.session() as session:
+                    startTs = datetime.now(timezone.utc)
+                    results = session.write_transaction(
+                        self._statementsAsTransaction, ctBlock.statements)
+                    print(results)
+                    endTs = datetime.now(timezone.utc)
+                    elapsedSec = um.dateDiff('sec', startTs, endTs)
+                    ctBlock.runTime = elapsedSec
+                    ctBlock.status = 'done'
+
+                    self.removeBlockFromWorkingQueue(
+                        ctBlock.instanceToSerial())
+                    self.cypherQueues.pushCtbToCompletedQ(ctBlock)
+                    return True
+            except CypherSyntaxError as e:
+                print('CypherSyntaxError')
+                print(repr(e))
+                ctBlock.numRetries += 1
+                ctBlock.status = 'CypherSyntaxError'
+                ctBlock.errors = repr(e)
+                self.removeBlockFromWorkingQueue(ctBlock.instanceToSerial())
+                self.cypherQueues.pushCtbToCompletedQ(ctBlock)
+
+                return False
+            except ServiceUnavailable as e:
+                print('ServiceUnavailable')
+                print(repr(e))
+                ctBlock.numRetries += 1
+                ctBlock.status = 'ServiceUnavailable'
+                ctBlock.errors = repr(e)
+                self.removeBlockFromWorkingQueue(ctBlock.instanceToSerial())
+                self.cypherQueues.pushCtbToWaitingQ(ctBlock)
+                return False
+            except ClientError as e:
+                print('ClientError')
+                print(repr(e))
+                ctBlock.numRetries += 1
+                ctBlock.status = 'ClientError'
+                ctBlock.errors = repr(e)
+                self.removeBlockFromWorkingQueue(ctBlock.instanceToSerial())
+                self.cypherQueues.pushCtbToCompletedQ(ctBlock)
+                return False
+            """except Exception as e:
+                print(repr(e))
+                self.removeBlockFromWorkingQueue(ctBlock.instanceToSerial())
+                self.cypherQueues.pushCtbToWaitingQ(ctBlock)
+                return False"""
 
     def _statementsAsTransaction(self, tx, statements):
         results = []
